b888d391a432f1e4f3aea31a14928cb789a83495
ibrahim.baltah <ibrahim.abbalta@gmail.com>
2020-11-04 14:40:10 +0100

    (tkinter package) how to create a menu and but staff in it.

--- Python_tkinter_Creating_Menus_1.py
+++ Python_tkinter_Creating_Menus_1.py
@@ -11,15 +11,15 @@ menu = Menu(root)
 root.config(menu=menu)
 
 subMenu = Menu(menu)
-menu.add_cascade(Label="File", menu=subMenu)
-subMenu.add_command(Label="New Project...", command=do_nothing)
-subMenu.add_command(Label="New...", command=do_nothing)
+menu.add_cascade(label="File", menu=subMenu)
+subMenu.add_command(label="New Project...", command=do_nothing)
+subMenu.add_command(label="New...", command=do_nothing)
 subMenu.add_separator()
-subMenu.add_command(Label="Exit", command=do_nothing)
+subMenu.add_command(label="Exit", command=do_nothing)
 
 editMenu = Menu(menu)
-menu.add_cascade(Label="Edit", menu=editMenu)
-editMenu.add_command(Label="Redo", command=do_nothing)
+menu.add_cascade(label="Edit", menu=editMenu)
+editMenu.add_command(label="Redo", command=do_nothing)
 
 
 root.mainloop()
